e6bdd6207629c52b20efd1c28adb22fa265b8dc0
unknown <miko@BIGNB012.intra.bigler.ch>
2014-01-03 10:36:51 +0100

    no thread, handle 00 months

--- memo.py
+++ memo.py
@@ -1,9 +1,7 @@
 import datetime
 import os
 import shutil
-import time
 import ui
-from thread import start_new_thread as snt
 
 nb024Path = "C:\\Dokumente und Einstellungen\\bizerba\\Desktop\\Datensicherungen"
 cardPath = "-:\\BACKUP\\"
@@ -18,54 +16,37 @@ cardSave = False
 card2Save = False
 netSave = False
 
-counter = 0
-
 def memToPc():
-    global counter
     cardSave = startLoad(cardPath)
     nb024Save = startLoad(nb024Path)
     year = "\\" + str(datetime.datetime.now().year) + "\\"
     for i in range(len(cardSave)):
-        counter += 1
         currPath = nb024Path + year + cardSave[i][:3]
-        snt(copyTo,(cardPath, cardSave[i], currPath,onError,onEnd))
-    while counter > 0:
-        time.sleep(2)
+        copyTo(cardPath, cardSave[i], currPath)
     cardSave = startLoad(cardPath)
     deleteDouble(cardPath, cardSave)
 
 def memToNet():
-    global counter
     cardSave = startLoad(cardPath)
     netSave = startLoad(netPath)
     for i in range(len(cardSave)):
-        counter += 1
-        snt(copyTo,(cardPath, cardSave[i], netPath,onError,onEnd))
-    while counter > 0:
-        time.sleep(2)
+        copyTo(cardPath, cardSave[i], netPath)
     netSave = startLoad(netPath)
     deleteDouble(netPath, netSave)
 
 def memToMem():
-    global counter
     # 1 -> 2
     cardSave = startLoad(cardPath)
     card2Save = startLoad(card2Path)
     for i in range(len(cardSave)):
-        counter += 1
-        snt(copyTo,(cardPath, cardSave[i], card2Path,onError,onEnd))
-    while counter > 0:
-        time.sleep(2)
+        copyTo(cardPath, cardSave[i], card2Path)
     card2Save = startLoad(card2Path)
     deleteDouble(card2Path, card2Save)
     # 2 -> 1
     cardSave = startLoad(cardPath)
     card2Save = startLoad(card2Path)
     for i in range(len(card2Save)):
-        counter += 1
-        snt(copyTo,(card2Path, card2Save[i], cardPath,onError,onEnd))
-    while counter > 0:
-        time.sleep(2)
+        copyTo(card2Path, card2Save[i], cardPath)
     cardSave = startLoad(cardPath)
     deleteDouble(cardPath, cardSave)
 
@@ -85,7 +66,7 @@ def deleteDouble(path, saves):
         if i == 0:
             continue
         if saves[i-1][:3] == saves[i][:3]:
-            if saves[i][4:6] == "12" and saves[i-1][4:6] == "01":
+            if saves[i][4:6] == "12" and (saves[i-1][4:6] == "01" or saves[i-1][4:6] == "00"):
                 ui.addText("entferne " + saves[i] + " behalte " + saves[i-1])
                 if doIt:
                     shutil.rmtree(path + saves[i])
@@ -93,27 +74,15 @@ def deleteDouble(path, saves):
                 ui.addText("entferne " + saves[i-1] + " behalte " + saves[i])
                 if doIt:
                     shutil.rmtree(path + saves[i-1])
+            if saves[i-1][4:6] == "00"
+                ui.addText("entferne " + saves[i-1]
+                if doIt:
+                    shutil.rmtree(path + saves[i-1])
 
-def copyTo(fromPath, aSave, toPath, onError, onEnd):
-    global counter
-    if aSave[4:6] != "12" and not os.path.exists(toPath + "\\" + aSave):
-        if doIt:
-            try:
+def copyTo(fromPath, aSave, toPath):
+        if not os.path.exists(toPath + "\\" + aSave):
+            ui.addText("kopiere " + aSave + " nach " + toPath[-33:])
+            if doIt:
                 shutil.copytree(fromPath + aSave, toPath + "\\" + aSave)
-            except:
-                onError()
-                return
-        onEnd(aSave + " nach " + toPath[-33:] + " kopiert")
-    else:
-        counter -= 1
-
-def onEnd(s):
-    global counter
-    ui.addText(s)
-    counter -= 1
-
-def onError():
-    ui.addText("Ein unerwarteter Fehler ist aufgetreten")
-    assert False
 
 ui.create()
