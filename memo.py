3ce29d0da0434f8665336747279a6ccdcd629551
Mirco Kocher <mirco.kocher@gmx.ch>
2014-04-14 11:52:11 +0200

    simple wincws backup script

--- memo.py
+++ memo.py
@@ -3,51 +3,18 @@ import os
 import shutil
 import ui
 
-nb023Path = "D:\\Datensicherungen"
-cardPath = "F:\\BACKUP\\"
-card2Path = "H:\\BACKUP\\"
-netPath = "I:\\Bizerba Sicherungen\\"
+locPath = "C:\\ProgramData\\BIZERBA\\WinCWS_SQL\\"
+locs = ["Basis", "Orello", "Lyss"]
+fName = "WINCWS.FDB"
+netPath = "I:\\Bizerba Sicherungen\\WinCWS DB Backups\\"
 
-doIt = False
-
-nb023Save = False
-cardSave = False
-card2Save = False
 netSave = False
+locSave = False
 
-def memToPc():
-    cardSave = startLoad(cardPath)
-    nb023Save = startLoad(nb023Path)
-    year = "\\" + str(datetime.datetime.now().year) + "\\"
-    for i in range(len(cardSave)):
-        currPath = nb023Path + year + cardSave[i][:3]
-        copyTo(cardPath, cardSave[i], currPath)
-    cardSave = startLoad(cardPath)
-    deleteDouble(cardPath, cardSave)
-
-def memToNet():
-    cardSave = startLoad(cardPath)
+def saveDB(i):
     netSave = startLoad(netPath)
-    for i in range(len(cardSave)):
-        copyTo(cardPath, cardSave[i], netPath)
-    netSave = startLoad(netPath)
-    deleteDouble(netPath, netSave)
-
-def memToMem():
-    # 1 -> 2
-    cardSave = startLoad(cardPath)
-    card2Save = startLoad(card2Path)
-    for i in range(len(cardSave)):
-        copyTo(cardPath, cardSave[i], card2Path)
-    card2Save = startLoad(card2Path)
-    deleteDouble(card2Path, card2Save)
-    # 2 -> 1
-    cardSave = startLoad(cardPath)
-    card2Save = startLoad(card2Path)
-    for i in range(len(card2Save)):
-        copyTo(card2Path, card2Save[i], cardPath)
-    cardSave = startLoad(cardPath)
-    deleteDouble(cardPath, cardSave)
+    locSave = startLoad(locPath)
+    copyTo(locPath + str(i) + locs[i-1] + "\\Database", fName, netPath + locs[i-1])
 
 def startLoad(path):
     saves = False
@@ -58,45 +25,13 @@ def startLoad(path):
         assert False
     return saves
 
-def deleteDouble(path, saves):
-    for i in range(len(saves)):
-        if i == 0:
-            continue
-        if saves[i-1][:3] <> saves[i][:3]:
-            continue
-        if saves[i][4] <> "0" and saves[i][4] <> "1":
-            continue
-        currMonth = datetime.datetime.now().month
-        if int(saves[i][4:6]) > currMonth: # e.g: 505-0102 and 505-1214 after new year
-            if os.path.isdir(path + saves[i-1]):
-                myRemove(path + saves[i], saves[i], saves[i-1])
-            else:
-                myRemove(path + saves[i-1], saves[i-1], saves[i])
-        else: # e.g: 505-0714 and 505-0721
-            if os.path.isdir(path + saves[i]):
-                myRemove(path + saves[i-1], saves[i-1], saves[i])
-            else:
-                myRemove(path + saves[i], saves[i], saves[i-1])
-
-def copyTo(fromPath, aSave, toPath):
-    currMonth = datetime.datetime.now().month
-    if (not os.path.exists(toPath + "\\" + aSave) and os.path.isdir(fromPath + "\\" + aSave)):
-        if ((aSave[4] <> "0" and aSave[4] <> "1") or int(aSave[4:6]) <= currMonth):
-            ui.addText("kopiere " + aSave + " nach " + toPath[-33:])
-            if doIt:
-                shutil.copytree(fromPath + aSave, toPath + "\\" + aSave)
-
-def myRemove(path, remName, keepName):
-    ui.addText("entferne " + remName + " behalte " + keepName)
-    if doIt:
-        if os.path.isdir(path):
-            shutil.rmtree(path)
-        if os.path.isfile(path):
-            os.remove(path)
-
-def tryRepair():
-    ui.addText("Versuche " + cardPath[:2] + " zu reparieren")
-    ui.addText("Bei Frage \"Ordner oder Verlorene Kette in Datei umwandeln\" mit \"J\" antworten")
-    os.system('chkdsk /f ' + cardPath[:2])
+def copyTo(fromPath, fName, toPath):
+    currYear = str(datetime.datetime.now().year)
+    currMonth = "0" + str(datetime.datetime.now().month)
+    currDay = "0" + str(datetime.datetime.now().day)
+    currDate = currYear + currMonth[:2] + currDay[:2]
+    ui.addText("kopiere " + fName + " nach " + toPath)
+    newName = fName[:-4] + " " + currDate + fName[-4:]
+    shutil.copy(fromPath + "\\" + fName, toPath + "\\" + newName)
 
 ui.create()
--- ui.py
+++ ui.py
@@ -6,7 +6,7 @@ import wx
 class MyFrame(wx.Frame):
 
     logger = None
-    l = ["Memocard 1 -> PC", "Memocard 1 -> Netz", "Memocard <-> Memocard", "Reparieren"]
+    l = ["Basis Sichern", "Lyss Sichern", "Orello Sichern"]
     
     def __init__(self, t):
         wx.Frame.__init__(self, None, title=t, size=(800,400))
@@ -17,30 +17,7 @@ class MyFrame(wx.Frame):
         grid = wx.GridBagSizer(hgap=5, vgap=5)
         hSizer = wx.BoxSizer(wx.HORIZONTAL)
 
-        # the combobox Control
-        driveList = ['E:', 'F:', 'G:', 'H:', 'I:', 'J:', 'K:']
-        # dev 1
-        self.textDrive1 = wx.StaticText(panel, label="Laufwerk Memocard 1")
-        grid.Add(self.textDrive1, pos=(1,0))
-        self.drive1 = wx.ComboBox(panel, size=(95, -1), choices=driveList, style=wx.CB_DROPDOWN)
-        self.drive1.SetValue(driveList[1])
-        grid.Add(self.drive1, pos=(1,1))
-        self.Bind(wx.EVT_COMBOBOX, self.EvtComboBox, self.drive1)
-        # dev 2
-        self.textDrive2 = wx.StaticText(panel, label="Laufwerk Memocard 2")
-        grid.Add(self.textDrive2, pos=(2,0))
-        self.drive2 = wx.ComboBox(panel, size=(95, -1), choices=driveList, style=wx.CB_DROPDOWN)
-        self.drive2.SetValue(driveList[3])
-        grid.Add(self.drive2, pos=(2,1))
-        self.Bind(wx.EVT_COMBOBOX, self.EvtComboBox2, self.drive2)
-
-        # Checkbox
-        self.doit = wx.CheckBox(panel, label="Richtig kopieren/entfernen?")
-        self.doit.SetValue(False)
-        grid.Add(self.doit, pos=(4,0), span=(1,2), flag=wx.BOTTOM, border=5)
-        self.Bind(wx.EVT_CHECKBOX, self.EvtCheckBox, self.doit)
-
-        # 4 Buttons
+        # 3 Buttons
         self.button1 =wx.Button(panel, size=(150,30), label=MyFrame.l[0])
         self.Bind(wx.EVT_BUTTON, self.OnClick1, self.button1)
         grid.Add(self.button1, pos=(6,0))
@@ -50,9 +27,6 @@ class MyFrame(wx.Frame):
         self.button3 =wx.Button(panel, size=(150,30), label=MyFrame.l[2])
         self.Bind(wx.EVT_BUTTON, self.OnClick3, self.button3)
         grid.Add(self.button3, pos=(8,0))
-        self.button4 =wx.Button(panel, size=(150,30), label=MyFrame.l[3])
-        self.Bind(wx.EVT_BUTTON, self.OnClick4, self.button4)
-        grid.Add(self.button4, pos=(11,0))
 
         # A multiline TextCtrl
         MyFrame.logger = wx.TextCtrl(panel, style=wx.TE_MULTILINE | wx.TE_READONLY)
@@ -61,57 +35,35 @@ class MyFrame(wx.Frame):
         hSizer.Add(MyFrame.logger, 1, wx.EXPAND)
         panel.SetSizerAndFit(hSizer)
 
-    def EvtComboBox(self, event):
-        if len(event.GetString()) < 2:
-            return
-        MyFrame.logger.AppendText("Memocard 1: \n")
-        memo.cardPath = event.GetString() + memo.cardPath[2:]
-        MyFrame.logger.AppendText(memo.cardPath + "\n")
-    def EvtComboBox2(self, event):
-        MyFrame.logger.AppendText("Memocard 2: \n")
-        memo.card2Path = event.GetString() + memo.card2Path[2:]
-        MyFrame.logger.AppendText(memo.card2Path + "\n")
     def OnClick1(self,event):
-        self.clicked(memo.memToPc, MyFrame.l[0])
+        self.clicked(memo.saveDB, 1, MyFrame.l[0])
     def OnClick2(self,event):
-        self.clicked(memo.memToNet, MyFrame.l[1])
+        self.clicked(memo.saveDB, 2, MyFrame.l[1])
     def OnClick3(self,event):
-        self.clicked(memo.memToMem, MyFrame.l[2])
-    def OnClick4(self,event):
-        self.clicked(memo.tryRepair, MyFrame.l[3])
-    def EvtCheckBox(self, event):
-        if memo.doIt:
-            memo.doIt = False
-            MyFrame.logger.AppendText("\nNur Vorschau\n\n")
-        else:
-            memo.doIt = True
-            MyFrame.logger.AppendText("\nRichtig kopieren und entfernen\n\n")
+        self.clicked(memo.saveDB, 3, MyFrame.l[2])
     def OnClose(self, event):
         loggerValue = MyFrame.logger.GetValue()
         if len(loggerValue) > 1:
             now = datetime.datetime.now()
             dateString = "%d%02d%02d" % (now.year, now.month, now.day)
             dateString += "%02d%02d%02d" % (now.hour, now.minute, now.second)
-            f = open("D:\\Memocard Backup Tool\\log\\" + dateString + ".txt", "w+")
+            f = open("D:\\WinCWS Backup Tool\\log\\" + dateString + ".txt", "w+")
             f.write(MyFrame.logger.GetValue())
             f.close()
         self.Destroy()
-    def clicked(self, f, s):
+    def clicked(self, f, i, s):
         MyFrame.logger.AppendText(s + "\n")
         start = time.time()
-        f()
+        f(i)
         totalTime = round((time.time() - start)*10)/10
-        if memo.doIt:
-            MyFrame.logger.AppendText("Fertig in " + str(totalTime) + " Sekunden\n")
-        else:
-            MyFrame.logger.AppendText("Fertig\n")
+        MyFrame.logger.AppendText("Fertig in " + str(totalTime) + " Sekunden\n")
 
 def addText(s):
     MyFrame.logger.AppendText(s + "\n")
 
 def create():
     app = wx.App(False)
-    top = MyFrame("Memocard Backup Tool")
+    top = MyFrame("WinCWS Backup Tool")
     favicon = wx.Icon("C:\Python27\DLLs\py.ico", wx.BITMAP_TYPE_ICO, 16, 16)
     top.SetIcon(favicon)
     top.Show()
